fb7d4db2027e5ad3fd02bd93e218a8c350ba16f7
GustavoSantosRodrigues <s.rodrigues.2050@hotmail.com>
2026-04-27 16:38:15 -0300

    feat: create categories section

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,25 +1,39 @@
+import bannerBlackFriday from './banner-black-friday.png'
+import corrida from './corrida.png'
 import creditCard from './CreditCard.png'
+import crownSimple from './CrownSimple.png'
+import cuidadosDeSaude from './cuidados-de-saude.png'
+import ferramentas from './ferramentas.png'
 import heart from './Heart.png'
+import iconSearch from './search.svg'
 import logoEconverse from './logo-econverse.png'
 import logout from './logout.png'
+import moda from './moda.png'
+import shieldCheck from './ShieldCheck.png'
 import shoppingCart from './ShoppingCart.png'
+import supermercados from './supermercados.png'
+import tecnologia from './tecnologia.png'
 import truck from './Truck.png'
 import userCircle from './UserCircle.png'
-import shieldCheck from './ShieldCheck.png'
-import crownSimple from './CrownSimple.png'
-import iconSearch from './search.svg'
-import bannerBlackFriday from './banner-black-friday.png'
+import whiskey from './whiskey.png'
 
 export const images = {
+  bannerBlackFriday,
+  corrida,
   creditCard,
+  crownSimple,
+  cuidadosDeSaude,
+  ferramentas,
   heart,
+  iconSearch,
   logoEconverse,
   logout,
+  moda,
+  shieldCheck,
   shoppingCart,
+  supermercados,
+  tecnologia,
   truck,
   userCircle,
-  shieldCheck,
-  crownSimple,
-  iconSearch,
-  bannerBlackFriday,
+  whiskey,
 }
